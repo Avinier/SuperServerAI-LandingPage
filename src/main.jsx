65d85e574492450ad54fca54d911d2e853aa82c2
Avinier <adisubu.2410@gmail.com>
2024-09-30 12:59:24 +0530

    added routing, changed something in header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Root from './routes/root.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import AboutPage from './routes/about.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Root/>
+  },
+  {
+    path: "/about",
+    element: <AboutPage/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
