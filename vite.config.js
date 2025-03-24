c00a33f6be32531a4d6292331bbe96e61b6c5a1a
Sinha-Aditya <98398594+Sinha-Aditya@users.noreply.github.com>
2025-03-24 19:22:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    // true
+    allowedHosts: [
+      "dev.quantumsenses.com",
+      "dj.quantumsenses.com",
+      "ac19f3d0-f27d-499b-ae4e-91985d3cc7d2-00-3iyy1k8fy6204.pike.replit.dev"
+    ]
+  },
+  build: {
+    sourcemap: process.env.NODE_ENV === 'development'
+  }
 });
